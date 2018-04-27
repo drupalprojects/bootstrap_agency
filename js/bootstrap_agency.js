b620f6374340745e7e3771561fe320334775a0d4
Joao Ventura <joao.ventura@wunder.io>
2018-04-27 11:03:08 +0200

    Code style fixes.

--- js/bootstrap_agency.js
+++ js/bootstrap_agency.js
@@ -1,8 +1,13 @@
+/**
+ * @file
+ * Javascript functions used by the bootstrap_agency theme.
+ */
+
 (function ($) {
   Drupal.behaviors.agencyTheme = {
     attach: function (context) {
-      // Replace cbpAnimatedHeader.js
-      $(window).scroll(function(){
+      // Replace cbpAnimatedHeader.js.
+      $(window).scroll(function () {
           if ($(this).scrollTop() > 200) {
               $('.navbar-default').addClass('navbar-shrink');
           }
@@ -12,7 +17,7 @@
       });
 
       // Hide the responsive menu when clicking in a menu item.
-      $('#navbar').find('.navbar-collapse ul li:not(.dropdown) a').click(function() {
+      $('#navbar').find('.navbar-collapse ul li:not(.dropdown) a').click(function () {
           $('#navbar button.navbar-toggle:visible').click();
       });
     }
